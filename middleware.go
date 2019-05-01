efae62a06bcd55f9b5430c7a70564cc7a1dc504e
Alexander Skafte <alexanderskafte@protonmail.com>
2019-05-02 00:33:56 +0200

    stop returning error from message handlers

--- middleware.go
+++ middleware.go
@@ -8,28 +8,24 @@ import (
 
 type Middleware func(MessageHandler) MessageHandler
 
+func (h MessageHandler) With(mw Middleware) MessageHandler {
+	return mw(h)
+}
+
 func Recovery(next MessageHandler) MessageHandler {
-	return func(d amqp.Delivery) error {
+	return func(d amqp.Delivery) {
 		defer func() {
 			if rvr := recover(); rvr != nil {
 				log.Println(rvr)
-				// http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			}
 		}()
-		_ = next(d)
-		return nil
+		next(d)
 	}
 }
 
-// TODO
 func Logging(next MessageHandler) MessageHandler {
-	return func(d amqp.Delivery) error {
+	return func(d amqp.Delivery) {
 		log.Println("Received message with ID", d.MessageId)
-		_ = next(d)
-		return nil
+		next(d)
 	}
-}
-
-func (h MessageHandler) With(mw Middleware) MessageHandler {
-	return mw(h)
 }
